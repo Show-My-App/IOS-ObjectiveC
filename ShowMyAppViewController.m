58a84b1375e56509f4705935f94d6f25a7354699
jfcontart <jfcontart@idemobi.com>
2019-06-04 14:55:15 +0200

    [STEP] design

--- ShowMyAppViewController.m
+++ ShowMyAppViewController.m
@@ -32,8 +32,8 @@
         [imageIcon setTintColor:[ShowMyAppInstance DesignColorBackground]];
         [imageIcon setImage:[[imageIcon image] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
         
-        [imageQRCode setBackgroundColor:[ShowMyAppInstance DesignColorBackground]];
-        [imageQRCode setTintColor:[ShowMyAppInstance DesignColor]];
+        [imageQRCode setBackgroundColor:[ShowMyAppInstance DesignColor]];
+        [imageQRCode setTintColor:[ShowMyAppInstance DesignColorBackground]];
         
         [imageQRCodeOverlay setBackgroundColor:[ShowMyAppInstance DesignColorBackground]];
         [imageQRCodeOverlay setTintColor:[ShowMyAppInstance DesignColor]];
@@ -58,67 +58,27 @@
       
         [ShowMyAppInstance InsertQRCode:imageQRCode];
         
-        
-        
 //        [QRCodeArea setTranslatesAutoresizingMaskIntoConstraints:NO];
         [QRCodeArea setTransform:CGAffineTransformMakeRotation(M_PI_4)];
-        
-        
-//        // Setup the animation
-//        [UIView beginAnimations:nil context:NULL];
-//        [UIView setAnimationDuration:60];
-//        [UIView setAnimationCurve:UIViewAnimationCurveLinear];
-//        [UIView setAnimationBeginsFromCurrentState:YES];
-//
-//        // The transform matrix
-//        CGAffineTransform transform = CGAffineTransformMakeRotation(2*M_PI);
-//        QRCodeArea.transform = transform;
-//
-//        // Commit the changes
-//        [UIView commitAnimations];
-        
-//        [QRCodeArea setAutoresizesSubviews:NO];
-//        QRCodeArea.alpha = 1.0;
-//        [UIView animateWithDuration:20
-//                         animations:^{QRCodeArea.alpha = 0.0;}
-//                         completion:^(BOOL finished){ [QRCodeArea removeFromSuperview]; }];
-//        [self rotateImageView];
-        
-//        [imageQRCode setAutoresizingMask:UIViewAutoresizingNone];
-//        [QRCodeArea setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
-//        imageQRCode.transform = CGAffineTransformRotate(imageQRCode.transform , -M_PI/2.0);
-//        [self runSpinAnimationOnView:QRCodeArea duration:60 repeat:true];
+        [self rotateImageView];
     }
 }
 
 - (void)rotateImageView
 {
     NSLog(@" %s line %d",__FUNCTION__, __LINE__);
+    [[QRCodeArea superview] setNeedsLayout];
     [UIView animateWithDuration:60 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
         NSLog(@" %s line %d",__FUNCTION__, __LINE__);
-//        [self->QRCodeArea setTransform:CGAffineTransformRotate(self->QRCodeArea.transform, M_PI_2)];
-//        [self->QRCodeArea setTransform:CGAffineTransformRotate(CGAffineTransformIdentity, M_PI_2)];
+//        [[self->QRCodeArea layer] setTransform:CATransform3DRotate(CATransform3DIdentity, M_PI_2, 0, 0, 1)];
+//        [self->QRCodeArea setTransform:CGAffineTransformRotate(self->QRCodeArea.transform,M_PI)];
 //        [[self->QRCodeArea superview] layoutIfNeeded];
-//        [[self view] layoutIfNeeded];
-        [self->QRCodeArea setTransform:CGAffineTransformTranslate(self->QRCodeArea.transform,320,320)];
-    }completion:^(BOOL finished){
+    } completion:^(BOOL finished) {
         if (finished) {
             NSLog(@" %s line %d",__FUNCTION__, __LINE__);
             [self rotateImageView];
         }
     }];
-}
-
-- (void) runSpinAnimationOnView:(UIView*)view duration:(CGFloat)duration repeat:(float)repeat {
-    CABasicAnimation* rotationAnimation;
-    rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-    rotationAnimation.fromValue = [NSNumber numberWithInt:0];
-    rotationAnimation.toValue = [NSNumber numberWithFloat: M_PI * 2.0];
-    rotationAnimation.duration = duration;
-    rotationAnimation.cumulative = YES;
-    rotationAnimation.repeatCount = repeat ? HUGE_VALF : 0;
-    
-    [view.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
 }
 
 - (void)Install:(ShowMyApp*) sShowMyAppInstance {
